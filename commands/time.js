f5af43413ee3b71c02aa4f308dadb037db62b7ce
Eduardo Metring <67509355+eduardometring@users.noreply.github.com>
2020-10-26 21:36:17 -0300

    💫 Time.js

--- commands/time.js
+++ commands/time.js
@@ -1,6 +1,7 @@
 const discord = require('discord.js')
 exports.run = async(client, message, args) => {
   let itBe = new discord.MessageEmbed()
+  .setFooter(`ok`)
   .setTimestamp()
   message.channel.send(itBe)
 }
